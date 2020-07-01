55f19ad365acaf47b754f98cb915ceae094bfbb2
HP <HP@192.168.43.70>
2020-07-01 11:48:51 +0530

    first commit

---
+++ GitTesting2/src/com/cjc/gittesting2/DemoGitTesting.java
@@ -0,0 +1,32 @@
+package com.cjc.gittesting2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class DemoGitTesting {
+	
+	public WebDriver driver;
+	
+	
+	@BeforeSuite
+	public void browser()
+	{
+		System.setProperty("webdriver.chrome.driver","F:\\cjc\\core\\Testing\\exefile\\chromedriver.exe");
+		
+		driver=new ChromeDriver();
+		System.out.println("chrome browser opened sucessfully");
+		
+	}
+	
+	@Test
+	public void url()
+	{
+		driver.get("https://www.facebook.com/");
+		System.out.println("URL sucessfully opened");
+	}
+    
+	
+	
+}
